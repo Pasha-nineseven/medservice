2a8e549fede6d0f839bc009948c2f2f632b17e8d
Pavel-nineseven <pavel@nineseven.ru>
2019-01-16 15:46:10 +0300

    doctor page-full

--- app/js/interface.js
+++ app/js/interface.js
@@ -3,6 +3,34 @@ $(document).ready(function() {
 	// $("body").on("click", ".test", function(e){
 	// 	e.preventDefault();
 	// })
+
+	//TOP-SEARCH
+    $("body").on("click", ".js-top-search__link", function(e){
+        $(".top-search__toggle").animate({
+	        width: 'toggle',
+	    });
+	});
+    $("body").on("click", ".js-top-search__close", function(e){
+        $(".top-search__toggle").animate({
+	        width: 'toggle',
+	    });
+	});
+
+
+	//MENU-MOBILE
+    $("body").on("click", ".js-menu-btn", function(e){
+        $(".menu-mobile").fadeIn();
+        setTimeout(function(){
+			$('body').addClass('hidden');
+		}, 400);
+	});
+	$("body").on("click", ".js-menu-mobile__close", function(e){
+		$('body').removeClass('hidden');
+        $(".menu-mobile").fadeOut();
+  //       setTimeout(function(){
+		// }, 400);
+	});
+
 	//TOOLTIP
 	if ($('.tooltip').length>0) {
 		$('.tooltip').tooltipster({
@@ -123,30 +151,15 @@ $(document).ready(function() {
 			infinite: false,
 			slidesToShow: 3,
   			slidesToScroll: 1,
-  	// 		responsive: [
-			//     {
-			//       breakpoint: 1000,
-			//       settings: {
-			//         slidesToShow: 3,
-			//         slidesToScroll: 1,
-			//       }
-			//     },
-			//     {
-			//       breakpoint: 768,
-			//       settings: {
-			//         slidesToShow: 2,
-			//         slidesToScroll: 1,
-			//       }
-			//     },
-			//     {
-			//       breakpoint: 550,
-			//       settings: {
-			//         slidesToShow: 1,
-			//         slidesToScroll: 1,
-			//         adaptiveHeight:true,
-			//       }
-			//     },
-			// ]
+  			responsive: [
+			    {
+			      breakpoint: 800,
+			      settings: {
+			        slidesToShow: 2,
+			        slidesToScroll: 1,
+			      }
+			    },
+			]
 	    });
 	};
 
@@ -226,16 +239,18 @@ function initializeContactMap() {
 
 
 // links pages
-// $('body').append(
-// 	'<div style="position: fixed; z-index: 1005; bottom: 0; right: 0; background: #fff; border: solid 1px #828286; width: 200px;"> \
-// 		<a href="javascript:void(0);" style="float: right;background:#ccc; color:#000; padding: 5px 10px; text-decoration: none; font-size: 16px" onclick="$(this).parent().hide()">Close X</a> \
-// 	<style> \
-// 		#pages { padding: 10px 20px 0 50px; font-size: 18px; } \
-// 		#pages a { text-decoration: none; } \
-// 		#pages li { margin: 5px 0; } \
-// 	</style> \
-// 	<ol id="pages"> \
-// 		<li><a href="about.html">About</a></li> \
-// 		<li><a href="index.html">Index</a></li> \
-// 	</ol> \
-// </div>');
+$('body').append(
+	'<div style="position: fixed; z-index: 1005; bottom: 0; right: 0; background: #fff; border: solid 1px #828286; width: 200px;"> \
+		<a href="javascript:void(0);" style="float: right;background:#ccc; color:#000; padding: 5px 10px; text-decoration: none; font-size: 16px" onclick="$(this).parent().hide()">Close X</a> \
+	<style> \
+		#pages { padding: 10px 20px 0 50px; font-size: 18px; } \
+		#pages a { text-decoration: none; } \
+		#pages li { margin: 5px 0; } \
+	</style> \
+	<ol id="pages"> \
+		<li><a href="text.html">Text</a></li> \
+		<li><a href="contacts.html">Contacts</a></li> \
+		<li><a href="doctor.html">Doctor</a></li> \
+		<li><a href="index.html">Index</a></li> \
+	</ol> \
+</div>');
